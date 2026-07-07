6da20d3656c85afe8e12abe861e142f44e3c4e10
freakyash11 <122809436+freakyash11@users.noreply.github.com>
2026-07-08 00:27:36 +0530

    feat: add DataTable component and CsvPreviewTable for CSV file data visualization

--- client/components/CsvPreviewTable.tsx
+++ client/components/CsvPreviewTable.tsx
@@ -44,7 +44,7 @@ export function CsvPreviewTable({ data, onConfirm, onCancel, isImporting }: CsvP
           </span>
         </div>
 
-        <div className="flex-1 overflow-hidden flex flex-col">
+        <div className="flex-1 min-h-0 overflow-hidden flex flex-col">
           <DataTable columns={columns} data={data.rows} emptyMessage="No rows to display." />
         </div>
       </div>
